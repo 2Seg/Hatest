fc0cecf8204b474b98fa3527003e513b9f9daa23
Nicolas Sicard <rashguir@gmail.com>
2019-01-15 16:45:19 +0100

    fix(traits dependencies): provide abstract classes to avoid using 4 traits inside one test class

--- src/Doctrine/Traits/CollectionTrait.php
+++ src/Doctrine/Traits/CollectionTrait.php
@@ -7,8 +7,6 @@ use Hatest\Traits\NameableTrait;
 
 trait CollectionTrait
 {
-    use NameableTrait;
-
     /**
      * Change property to camel case for methods
      *
@@ -131,5 +129,30 @@ trait CollectionTrait
         $this->assertInstanceOf(get_class($object), $object->$setter($element));
         $this->assertInstanceOf(get_class($object), $object->$remove($element));
         $this->assertFalse($object->$has($element));
+    }
+
+    /**
+     * @param string $operation
+     * @param string $propertyName
+     * @param array $options
+     *
+     * @return string
+     */
+    public function getFunctionName($operation, $propertyName, array $options = [])
+    {
+        switch ($operation) {
+            case 'get':
+                return key_exists('getter', $options) ? $options['getter'] : 'get'.$propertyName;
+            case 'add':
+                return key_exists('adder', $options) ? $options['adder'] : 'add'.$propertyName;
+            case 'remove':
+                return key_exists('remover', $options) ? $options['remover'] : 'remove'.$propertyName;
+            case 'has':
+                return key_exists('haser', $options) ? $options['haser'] : 'has'.$propertyName;
+            case 'set':
+                return key_exists('setter', $options) ? $options['setter'] : 'set'.$propertyName;
+        }
+
+        return '';
     }
 }
--- src/Traits/SetterTrait.php
+++ src/Traits/SetterTrait.php
@@ -4,8 +4,6 @@ namespace Hatest\Traits;
 
 trait SetterTrait
 {
-    use NameableTrait;
-
     /**
      * @dataProvider providerGetterAndSetter
      *
@@ -39,5 +37,30 @@ trait SetterTrait
                 $property
             )
         );
+    }
+
+    /**
+     * @param string $operation
+     * @param string $propertyName
+     * @param array $options
+     *
+     * @return string
+     */
+    public function getFunctionName($operation, $propertyName, array $options = [])
+    {
+        switch ($operation) {
+            case 'get':
+                return key_exists('getter', $options) ? $options['getter'] : 'get'.$propertyName;
+            case 'add':
+                return key_exists('adder', $options) ? $options['adder'] : 'add'.$propertyName;
+            case 'remove':
+                return key_exists('remover', $options) ? $options['remover'] : 'remove'.$propertyName;
+            case 'has':
+                return key_exists('haser', $options) ? $options['haser'] : 'has'.$propertyName;
+            case 'set':
+                return key_exists('setter', $options) ? $options['setter'] : 'set'.$propertyName;
+        }
+
+        return '';
     }
 }
